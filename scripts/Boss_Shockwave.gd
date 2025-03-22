d9f0f9eeb257929be2895df11d8aefdc1b7aa959
Pjotr Nikitin <pnikitin1@sheffield.ac.uk>
2025-03-22 11:01:32 +0000

    timer is oneshot

--- scripts/Boss_Shockwave.gd
+++ scripts/Boss_Shockwave.gd
@@ -5,7 +5,7 @@ func Exit():
 	pass
 
 func Enter():
-	pass
+	$windup.start()
 	
 func Update(_delta: float):
 	pass
@@ -13,7 +13,5 @@ func Update(_delta: float):
 func Physics_Update(_delta: float):
 	pass
 	
-
-
 func _on_windup_timeout():
 	pass # Replace with function body.
