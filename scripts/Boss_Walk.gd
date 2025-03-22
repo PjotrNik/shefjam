5fdf6175d97ddd1b2efebc2e78bb800b9a1908c7
Pjotr Nikitin <pnikitin1@sheffield.ac.uk>
2025-03-22 10:25:27 +0000

    Boss can switch to lob state

--- scripts/Boss_Walk.gd
+++ scripts/Boss_Walk.gd
@@ -10,8 +10,12 @@ func Exit():
 
 func Enter():
 	$"../..".current_speed = SPEED
-	#get player location
+	#Decide when to lob
+	var rng = RandomNumberGenerator.new()
+	var time = rng.randf_range(1.5, 3)
+	$lob_timer.start(time)
 	
+	#get player location
 	player = get_tree().get_first_node_in_group("Player")
 	if player.position.x > $"../..".position.x:
 		$"../..".direction = 1
@@ -33,3 +37,7 @@ func Update(_delta: float):
 	
 func Physics_Update(_delta: float):
 	pass
+
+
+func _on_lob_timer_timeout():
+	Transitioned.emit(self, "Boss_Lob")
