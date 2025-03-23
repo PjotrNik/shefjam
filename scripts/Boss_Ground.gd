db9769f4961b22ba337c6c0a23fb9cb0f9865598
PjotrNik <159441557+PjotrNik@users.noreply.github.com>
2025-03-23 01:44:58 +0000

    ground thorn attack works

--- scripts/Boss_Ground.gd
+++ scripts/Boss_Ground.gd
@@ -1,11 +1,14 @@
 extends State
 class_name Boss_Ground
+var roots = preload("res://scenes/roots.tscn")
 
 func Exit():
 	pass
 
 func Enter():
-	print ("About to attack whole ground")
+	print ("About to spawn roots")
+	$"../../AnimatedSprite2D".play("windup_roots")
+	$windup.start()
 	
 func Update(_delta: float):
 	pass
@@ -13,3 +16,14 @@ func Update(_delta: float):
 func Physics_Update(_delta: float):
 	pass
 	
+
+#We do things here
+func _on_windup_timeout():
+	print ("roots spawning")
+	for x in range(30): 
+		print("spawned roots")
+		var instance = roots.instantiate()
+		add_child(instance)
+		instance.global_position.x = 128 + (x * 32)
+		instance.global_position.y = 512
+		
